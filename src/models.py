2e2b0db51e38ca795d4e15c5c94e17e41a990f2f
josefrometa <73325073+josefrometa@users.noreply.github.com>
2023-10-25 07:48:26 +0000

    proyecto finalizado? se agregaron mas tablas y se establecieron las relaciones entre las mismas

--- src/models.py
+++ src/models.py
@@ -19,9 +19,9 @@ class User(Base):
 
 class Planets(Base):
     __tablename__ = 'planets'
-
     id = Column(Integer, primary_key=True)
     planet_name = Column(String(80), unique=True)
+    planet_of_birth = relationship('Characters_Planets', uselist=True, backref="planets")
     favorite = relationship("Favorite", uselist=True, backref="planets")
 
 
@@ -30,13 +30,37 @@ class Characters(Base):
     id = Column(Integer, primary_key=True)
     character_name = Column(String(50), nullable=False)
     eye_color = Column(String(20), nullable=False)
+    planet_of_birth = relationship('Characters_Planets', uselist=True, backref="characters")
+    characters_lightsaber = relationship("Characters_LightSaber", uselist=True, backref="characters")
+    favorite = relationship("Favorite", uselist=True, backref="characters")
 
+class Characters_Planets(Base):
+    __tablename__ = 'characters_planets'
+    id = Column(Integer, primary_key=True)
+    characters_id = Column(Integer(), ForeignKey('characters.id'))
+    planets_id = Column(Integer(), ForeignKey('planets.id'))
+
+class LightSaber(Base):
+    __tablename__ = 'lightsaber'
+    id = Column(Integer, primary_key=True)
+    lightsaber_color = Column(String(30), nullable=False)
+    character_lightsaber = relationship("Characters_LightSaber", uselist=True, backref="lightsaber")
+    favorite = relationship("Favorite", uselist=True, backref="lightsaber")
+
+class Character_LightSaber(Base):
+    __tablename__ = 'character_lightsaber'
+    id = Column(Integer(), primary_key=True)
+    characters_id = Column(Integer(), ForeignKey('characters.id'))
+    lightsaber_id = Column(Integer(), ForeignKey('lightsaber.id'))
 
 class Favorite(Base):
     __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
     use_id = Column(Integer(), ForeignKey("user.id"))
     planets_id = Column(Integer(), ForeignKey("planets.id"))
+    characters_id = Column(Integer(), ForeignKey("characters.id"))
+    lightsaber_id = Column(Integer(), ForeignKey("lightsaber.id"))
+
 
     def to_dict(self):
         return {}
